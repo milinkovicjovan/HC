deef845c95868020333145869476dc9233ab5f79
Jovan Milinkovic <jovan@Jovans-MacBook-Pro.local>
2024-08-13 01:34:54 +0200

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "new-order": ['"new-order"', "sans-serif"],
+      },
+      colors: {
+        pinkCustom: "#FFDDEE",
+        redCustom: "#E05F7E",
+        grayCustom: "#E9E9E9",
+        blueCustom: "#E6F0FF",
+      },
+    },
+  },
+  plugins: [],
+};
